8dcb923b5b5b1ae7e5219b9abd1042567ad28cf8
andygao82 <freedomandy82@gmail.com>
2021-02-17 21:19:28 +1100

    in prog

---
+++ wp-content/themes/marco/page-home.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Home Page */
+get_header();
+$img_path = get_template_directory_uri( '/' );
+
+?>
+
+<main>
+    <div class="banner banner-main banner-home">
+        banner
+    </div>
+</main>
+
+<?php get_footer(); ?>
