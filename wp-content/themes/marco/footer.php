4476d4cce26623bcdfefd9cdc57fa70e4fc75cdc
andygao82 <freedomandy82@gmail.com>
2021-02-19 22:29:25 +1100

    in prog

--- wp-content/themes/marco/footer.php
+++ wp-content/themes/marco/footer.php
@@ -60,18 +60,21 @@
 							<span>8:00 AM - 6:00 PM</span>
 						</div>
 						<div class="footer-newsletter">
+							<h3>Subscribe Our Newsletter</h3>
 							<!-- Begin Mailchimp Signup Form -->
 							<div id="mc_embed_signup">
 								<form action="https://marcofinefoods.us7.list-manage.com/subscribe/post?u=3229aeba97f46ad90350657c3&amp;id=e9a467706e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 									<div id="mc_embed_signup_scroll">
-										<div class="mc-field-group">
-											<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-											</label>
-											<input type="text" value="" name="FNAME" class="required" id="mce-FNAME"  placeholder="First Name" >
-										</div>
-										<div class="mc-field-group">
-											<label for="mce-LNAME">Last Name </label>
-											<input type="text" value="" name="LNAME" class="" id="mce-LNAME"  placeholder="Last Name" >
+										<div class="mc-fields">
+											<div class="mc-field-group">
+												<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+												</label>
+												<input type="text" value="" name="FNAME" class="required" id="mce-FNAME"  placeholder="First Name" >
+											</div>
+											<div class="mc-field-group">
+												<label for="mce-LNAME">Last Name </label>
+												<input type="text" value="" name="LNAME" class="" id="mce-LNAME"  placeholder="Last Name" >
+											</div>
 										</div>
 										<div class="mc-field-group">
 											<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
@@ -83,7 +86,7 @@
 											<div class="response" id="mce-success-response" style="display:none"></div>
 										</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 										<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3229aeba97f46ad90350657c3_e9a467706e" tabindex="-1" value=""></div>
-										<div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+										<div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="footer-submit"></div>
 									</div>
 								</form>
 							</div>
