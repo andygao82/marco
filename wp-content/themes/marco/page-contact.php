ad6cb5dab767d37120906b85bb55f0efc7005642
andygao82 <freedomandy82@gmail.com>
2021-02-23 22:14:49 +1100

    contact page

---
+++ wp-content/themes/marco/page-contact.php
@@ -0,0 +1,60 @@
+<?php
+    /* Template Name: Contact Page */
+    get_header();
+    $img_path = get_template_directory_uri( '/' );
+    $size = 'full';
+
+?>
+<main>
+    <div class="banner banner-main banner-contact banner-section" style="background-image: url('<?= get_field('banner_image')?>')">
+        <div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="wrapper">
+						<h1><?= the_title() ?></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+	<div class="main-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-7">
+					<div class="wrapper">
+						<div class="contact-form">
+							<h2>Get In Touch</h2>
+							<?= do_shortcode('[contact-form-7 id="167" title="Get In Touch"]')?>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-5">
+					<div class="wrapper wrapper-right">
+						<div class="contact-info">
+							<h3>Store Hours -</h3>
+							<div>
+								Monday - Saturday: 8:00 AM - 8:00 PM<br>
+								Sunday: 8:00 AM - 6:00 PM
+							</div>
+						</div>
+						<div class="contact-info">
+							<h3>Store Hours -</h3>
+							<div>Shop 8, Reservoir Cental<br>
+								830 Plenty Road, Reservoir VIC 3076
+							</div>
+						</div>
+						<div class="contact-info">
+							<h3>Contact Information -</h3>
+							<div>Phone: +61 3 8888 8888<br>
+								Email: info@marcofinefoods.com.au								
+							</div>
+						</div>
+						<a href="#" target="_blank" class="button get-direction">Get Direction</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php get_footer(); ?>
